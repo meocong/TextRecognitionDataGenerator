e997aec842f8bbbd18a7965d649fbad749419444
Hoang Viet <vieritolove@gmail.com>
2018-09-30 21:46:38 +0700

    increase prop

--- TextRecognitionDataGenerator/data_generator.py
+++ TextRecognitionDataGenerator/data_generator.py
@@ -158,7 +158,7 @@ class FakeTextDataGenerator(object):
                     kernel = np.ones((x, x), np.uint8)
                     im_arr = np.array(rotated_img)
                     erode = cv2.erode(im_arr, kernel, iterations=1)
-                    prob = np.random.choice([0.1, 0.2, 0.3], p=[0.5, 0.4, 0.1])
+                    prob = np.random.choice([0.1, 0.2, 0.3], p=[0.8, 0.19, 0.01])
                     mask = np.random.choice(2, im_arr.shape, p=[1 - prob, prob]).astype('uint8')
                     im_arr[mask > 0] = erode[mask > 0]
                     rotated_img = Image.fromarray(im_arr)
@@ -168,7 +168,7 @@ class FakeTextDataGenerator(object):
                         ## random pixel discard
                         # print("lol")
                         im_arr = np.array(rotated_img)
-                        prob = np.random.choice([0.1, 0.15, 0.25], p=[0.5, 0.35, 0.15])
+                        prob = np.random.choice([0.1, 0.15, 0.25], p=[0.8, 0.15, 0.05])
                         mask = np.random.choice(2, im_arr.shape, p=[1 - prob, prob]).astype('uint8')
                         im_arr[mask > 0] = 255
                         # im_arr = np.clip(im_arr, 0, 255).astype('uint8')
